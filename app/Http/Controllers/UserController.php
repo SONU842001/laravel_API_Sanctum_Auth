4ece229c4cdc65163b9620a5954408f04499e896
SONU842001 <54150646+SONU842001@users.noreply.github.com>
2022-04-13 20:00:41 +0530

    Adding login method to login user publicly

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -37,4 +37,29 @@ class UserController extends Controller
             'message' => 'Successfully logged out!!'
         ],200);
     }
+
+    // yaha where clause me email jo hai o column hai or $request->email jo hai o user dalega o hai
+
+    public function login(Request $request){
+        $request->validate([
+            'email'=>'required|email',
+            'password'=>'required',
+        ]);
+           $user = User::where('email',$request->email)->first();
+
+           if(!$user || !Hash::check($request->password,$user->password)){
+               return response([
+                   'message' => 'The provided credentials are incorrect.'
+               ],401);
+           }
+
+           else {
+            $token =$user->createToken('myToken')->plainTextToken;
+
+            return response([
+                'user'=>$user,
+                'token'=>$token,
+            ],200);
+           }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -22,6 +22,8 @@ use App\Http\Controllers\UserController;
 
 
 Route::post('/register',[UserController::class,'register']);
+Route::post('/login',[UserController::class,'login']);
+
 
 //Protected Routes
 
